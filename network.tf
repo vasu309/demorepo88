c76a9946afd6a77bbda22230e4892adcb61babd7
Saichandu <your-email@gmail.com>
2026-09-22 10:43:59 +0530

    added subnet code

--- network.tf
+++ network.tf
@@ -1,6 +1,13 @@
-azurerm_resource_virtual "name" {
+azurerm_resource_virtual_network "name" {
     name = "vnet"
     address_space = ["10.1.0.0/16"]
     azurerm_resource_name = "rg"
     location = "eastus"
+}
+
+resource "azurerm_subnet" "subnet" {
+    name = "subnet01"
+    address_prefixes = ["10.1.1.0/24"]
+    azurerm_resource_name = "rg"
+    virtual_network_name = azurerm_resource_virtual_network.name.name
 }
